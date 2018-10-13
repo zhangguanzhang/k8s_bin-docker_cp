cd9527c7e09b2b417c3eae2e4c45ddcc7fe4b9e3
zhangguanzhang <zhangguanzhang@qq.com>
2018-10-13 06:45:52 +0000

    Synchronizing completion at 2018-10-13

--- bash/full.sh
+++ bash/full.sh
@@ -9,17 +9,6 @@ sync_class_list=(
     )
 
 tag_list=(
-    kube-apiserver
-    kube-controller-manager
-    kube-proxy
-    kube-scheduler
-    kubectl
-    kubelet
-    apiextensions-apiserver
-    cloud-controller-manager
-    hyperkube
-    kubeadm
-    mounter
     full
     kube
     base
@@ -29,6 +18,7 @@ tag_list=(
 full::sync(){
     local tag=$1-full
     [ "$( hub_tag_exist $tag )" == null ] && {
+        sudo cp /$save_name .
         cat>Dockerfile<<-EOF
         FROM zhangguanzhang/alpine
         COPY $save_name /
@@ -36,33 +26,27 @@ EOF
         docker build -t zhangguanzhang/$img_name:$tag .
         docker push zhangguanzhang/$img_name:$tag
         echo zhangguanzhang/$img_name:$1-full > $CUR_DIR/tag/$tag
+        rm -f $save_name
     } || :
 }
 
 # version
 single::sync(){
+    du -shx *
     files=(
-        kube-apiserver
-        kube-controller-manager
-        kube-proxy
-        kube-scheduler
-        kubectl
-        kubelet
-        apiextensions-apiserver
-        cloud-controller-manager
-        hyperkube
-        kubeadm
-        mounter
-        )
+        $(sudo tar ztf /$save_name | grep -Po 'kubernetes/server/bin/\K[^.]+$')
+    )
     for file in ${files[@]};do
-        [[ "$( hub_tag_exist $1-$file )" == null && -f kubernetes/server/bin/$file ] && {
+        [ "$( hub_tag_exist $1-$file )" == null ] && {
+            sudo tar -zxvf /$save_name  --strip-components=3  kubernetes/server/bin/$file
             cat>Dockerfile<<-EOF
             FROM zhangguanzhang/alpine
-            COPY kubernetes/server/bin/$file /
+            COPY $file /
 EOF
             docker build -t zhangguanzhang/$img_name:$1-$file .
             docker push zhangguanzhang/$img_name:$1-$file
             echo zhangguanzhang/$img_name:$1-$file > $CUR_DIR/tag/$1-$file
+            rm -f $file
         } || :
     done
 }
@@ -81,8 +65,8 @@ kube::sync(){
         kubeadm
         )
     [ "$( hub_tag_exist $tag )" == null ] && {
-        rm -f $save_name
-        tar zcvf $save_name kubernetes/server/bin/{$( paste -sd ','< <(xargs -n1<<<"${files[@]}") )}
+        sudo tar -zxvf /$save_name  --strip-components=3  $( sed 's#^#kubernetes/server/bin/#' <(xargs -n1<<<"${files[@]}") )
+        tar zcvf $save_name "${files[@]}"
         cat>Dockerfile<<-EOF
         FROM zhangguanzhang/alpine
         COPY $save_name /
@@ -90,7 +74,7 @@ EOF
         docker build -t zhangguanzhang/$img_name:$tag .
         docker push zhangguanzhang/$img_name:$tag
         echo zhangguanzhang/$img_name:$tag > $CUR_DIR/tag/$tag
-        rm -f $save_name && cp zhangguanzhang $save_name
+        rm -f $save_name "${files[@]}"
     } || :
 }
 
@@ -105,8 +89,8 @@ base::sync(){
         kubelet
         )
     [ "$( hub_tag_exist $tag )" == null ] && {
-        rm -f $save_name
-        tar zcvf $save_name kubernetes/server/bin/{$( paste -sd ','< <(xargs -n1<<<"${files[@]}") )}
+        sudo tar -zxvf /$save_name  --strip-components=3  $( sed 's#^#kubernetes/server/bin/#' <(xargs -n1<<<"${files[@]}") )
+        tar zcvf $save_name "${files[@]}"
         cat>Dockerfile<<-EOF
         FROM zhangguanzhang/alpine
         COPY $save_name /
@@ -114,31 +98,27 @@ EOF
         docker build -t zhangguanzhang/$img_name:$tag .
         docker push zhangguanzhang/$img_name:$tag
         echo zhangguanzhang/$img_name:$tag > $CUR_DIR/tag/$tag
-        rm -f $save_name && cp zhangguanzhang $save_name
+        rm -f $save_name "${files[@]}"
     } || :
 }
 
 
 main(){
+    : ${save_name:=kubernetes-server-linux-amd64.tar.gz}
+
     cd $CUR_DIR/temp
     while read version;do
-        for list in "${tag_list[@]}";do
-            [ "$( hub_tag_exist $version-$list )" == null ] && {
-                printf -v version_url_download "$url_format" $version
-                save_name=${version_url_download##*/}
-                wget $version_url_download -O $save_name &>/dev/null
-                tar -zxvf $save_name
-                break
-            }
+        printf -v version_url_download "$url_format" $version
+        save_name=${version_url_download##*/}
+        sudo wget $version_url_download -O /$save_name &>/dev/null
+        
+        for run in ${sync_class_list[@]};do
+            $run::sync $version
+            [[ $(df -h| awk  '$NF=="/"{print +$5}') -ge "$max_per" ]] && docker image prune -f || :
+            [ $(( (`date +%s` - start_time)/60 )) -gt 47 ] && git_commit
         done
-        [ -n "version_url_download" ] && {
-            cp $save_name zhangguanzhang
-            for run in ${sync_class_list[@]};do
-                $run::sync $version
-                [[ $(df -h| awk  '$NF=="/"{print +$5}') -ge "$max_per" ]] && docker image prune -f || :
-            done
-            rm -rf $save_name kubernetes/ zhangguanzhang
-        }
+        sudo rm -rf $save_name /$save_name kubernetes/ 
+
     done < $version_file
 
     cd $CUR_DIR
