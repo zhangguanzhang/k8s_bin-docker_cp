dd63af8971ae5c68f6ec2d5767e0806314417efe
zhangguanzhang <zhangguanzhang@qq.com>
2018-10-13 18:01:03 +0800

    base version

---
+++ bash/full.sh
@@ -0,0 +1,128 @@
+#!/bin/bash
+
+# need to run,don't change the sort
+sync_class_list=(
+    full
+    single
+    kube
+    base
+    )
+
+# version
+full::sync(){
+    local tag=$1-full
+    [ "$( hub_tag_exist $tag )" == null ] && {
+        sudo cp /$save_name .
+        cat>Dockerfile<<-EOF
+        FROM zhangguanzhang/alpine
+        COPY $save_name /
+EOF
+        docker build -t zhangguanzhang/$img_name:$tag .
+        docker push zhangguanzhang/$img_name:$tag
+        echo zhangguanzhang/$img_name:$1-full > $CUR_DIR/tag/$tag
+        rm -f $save_name
+    } || :
+}
+
+# version
+single::sync(){
+    du -shx *
+    files=(
+        $(sudo tar ztf /$save_name | grep -Po 'kubernetes/server/bin/\K[^.]+$')
+    )
+    for file in ${files[@]};do
+        [ "$( hub_tag_exist $1-$file )" == null ] && {
+            sudo tar -zxvf /$save_name  --strip-components=3  kubernetes/server/bin/$file
+            cat>Dockerfile<<-EOF
+            FROM zhangguanzhang/alpine
+            COPY $file /
+EOF
+            sudo docker build -t zhangguanzhang/$img_name:$1-$file .
+            docker push zhangguanzhang/$img_name:$1-$file
+            echo zhangguanzhang/$img_name:$1-$file > $CUR_DIR/tag/$1-$file
+            rm -f $file
+        } || :
+    done
+}
+
+# version
+kube::sync(){
+    local tag=$1-kube local_kube_file=()
+    files=(
+        kube-apiserver
+        kube-controller-manager
+        kube-proxy
+        kube-scheduler
+        kubectl
+        kubelet
+        hyperkube
+        kubeadm
+        )
+    [ "$( hub_tag_exist $tag )" == null ] && {
+        
+        sudo tar -zxvf /$save_name  --strip-components=3  kubernetes/server/bin/
+        for file in ${files[@]};do
+            [ -f $file ] && local_kube_file+=($file)
+        done
+        tar zcvf $save_name "${local_kube_file[@]}"
+        rm -f $(ls -1I $save_name)
+        cat>Dockerfile<<-EOF
+        FROM zhangguanzhang/alpine
+        COPY $save_name /
+EOF
+        docker build -t zhangguanzhang/$img_name:$tag .
+        docker push zhangguanzhang/$img_name:$tag
+        echo zhangguanzhang/$img_name:$tag > $CUR_DIR/tag/$tag
+        rm -f $save_name 
+    } || :
+}
+
+base::sync(){
+    local tag=$1-base
+    files=(
+        kube-apiserver
+        kube-controller-manager
+        kube-proxy
+        kube-scheduler
+        kubectl
+        kubelet
+        )
+    [ "$( hub_tag_exist $tag )" == null ] && {
+        sudo tar -zxvf /$save_name  --strip-components=3  $( sed 's#^#kubernetes/server/bin/#' <(xargs -n1<<<"${files[@]}") )
+        tar zcvf $save_name "${files[@]}"
+        rm -f ${files[@]}
+        cat>Dockerfile<<-EOF
+        FROM zhangguanzhang/alpine
+        COPY $save_name /
+EOF
+        docker build -t zhangguanzhang/$img_name:$tag .
+        docker push zhangguanzhang/$img_name:$tag
+        echo zhangguanzhang/$img_name:$tag > $CUR_DIR/tag/$tag
+        rm -f $save_name
+    } || :
+}
+
+
+main(){
+    : ${save_name:=kubernetes-server-linux-amd64.tar.gz}
+
+    cd $CUR_DIR/temp
+    while read version;do
+        printf -v version_url_download "$url_format" $version
+        save_name=${version_url_download##*/}
+        sudo wget $version_url_download -O /$save_name &>/dev/null
+        
+        for run in ${sync_class_list[@]};do
+            $run::sync $version
+            [[ $(df -h| awk  '$NF=="/"{print +$5}') -ge "$max_per" ]] && docker image prune -f || :
+            [ $(( (`date +%s` - start_time)/60 )) -gt 47 ] && git_commit
+        done
+        sudo rm -rf $save_name /$save_name kubernetes/ 
+
+    done < $CUR_DIR/$version_file
+
+    cd $CUR_DIR
+    rm -rf temp/* 
+}
+
+main
---
+++ main.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+
+export CUR_DIR version_file MY_REPO img_name max_per url_format
+
+readonly CUR_DIR=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+max_per=70
+shell_dir='bash'
+version_file=version_file
+MY_REPO=zhangguanzhang
+img_name=k8s_bin
+url_format='https://dl.k8s.io/%s/kubernetes-server-linux-amd64.tar.gz'
+
+
+git_init(){
+    git config --global user.name "zhangguanzhang"
+    git config --global user.email zhangguanzhang@qq.com
+    git remote rm origin
+    git remote add origin git@github.com:zhangguanzhang/k8s_bin-docker_cp.git
+    git pull
+    if git branch -a |grep 'origin/develop' &> /dev/null ;then
+        git checkout develop
+        git pull origin develop
+        git branch --set-upstream-to=origin/develop develop
+    else
+        git checkout -b develop
+        git pull origin develop
+    fi
+}
+
+git_commit(){
+     local COMMIT_FILES_COUNT=$(git status -s|wc -l)
+     local TODAY=$(date +%F)
+     if [ "$COMMIT_FILES_COUNT" -ne 0 ];then
+        git add -A
+        git commit -m "Synchronizing completion at $TODAY"
+        git push -u origin develop
+     fi
+}
+
+# tag
+hub_tag_exist(){
+    curl -s https://hub.docker.com/v2/repositories/${MY_REPO}/${img_name}/tags/$1/ | jq -r .name
+}
+
+
+main(){
+    [ -z "$start_time" ] && start_time=$(date +%s)
+
+    sudo cp -r bash /
+    git_init
+    mkdir -p temp tag
+    sudo  rm -rf bash $version_file README.md ;yes|sudo  cp -r /bash .
+    wget https://raw.githubusercontent.com/zhangguanzhang/k8s_bin-docker_cp/master/$version_file -O $version_file
+    wget https://raw.githubusercontent.com/zhangguanzhang/k8s_bin-docker_cp/master/README.md
+    ls -l;ls -l temp
+#     while true;do
+#         curl -sX GET https://api.github.com/repos/zhangguanzhang/gcr.io/contents/gcr.io/google_containers/kube-apiserver-amd64?ref=develop |
+#             jq -r '.[].name' | grep -P 'v[\d.]+$' | sort -t '.' -n -k 2 &> $version_file
+#         [ "$(perl -nle 'END{print $.}'l $version_file)" -eq 10 ] && break
+#     done
+
+    while read shell_file;do
+        source $shell_file
+    done < <(find $shell_dir -type f -name '*.sh')
+    rm -rf temp
+    git_commit
+}
+
+main
+
